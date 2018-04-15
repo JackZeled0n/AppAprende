9fa2b8cafa015bb84cd8ee81fdd9f8ce22a035ae
Jake Antonio Ruiz Zeledón <jackiezeledon@gmail.com>
2018-04-14 23:20:19 -0600

    Incorporar proyecto a repositorio remoto

---
+++ login.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en" class="full-height">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>AppAprende</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+
+<body  class="navy-blue-skin"  id="pattern-lp">
+    
+    <!--Navegación principal-->
+    <header>
+
+        <!--Menu-->
+        <nav class="navbar navbar-login navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
+            <div class="container" >
+                <a class="navbar-brand" href="index.html">AppAprende</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Iniciar Sesión</a>
+                        </li>                                              
+                    </ul>                    
+                </div>
+            </div>
+        </nav>
+        <!--/.Menu-->
+
+        <!--Formulario-->
+        <section class="view intro-1 hm-black-strong">
+            <div class="full-bg-img flex-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-5 col-lg-6 col-md-10 col-sm-12 mx-auto mt-lg-5">
+
+                            <div class="card wow fadeIn  pattern-6" data-wow-delay="0.3s">
+                                <div class="card-body">
+
+                                    <!--Header-->
+                                    <div class="form-header" style="background-color:#314267;">
+                                        <h3><i class="fa fa-user mt-2 mb-2" ></i> Iniciar Sesión:</h3>
+                                    </div>
+
+                                    <!--Body-->
+                                    <form action="ld-android.html" method="">
+                                        <div class="md-form">
+                                            <i class="fa fa-user prefix" style="color:#314267;"></i>
+                                            <input placeholder="Usuario" type="text" id="nombreusuariologin" class="form-control white-text">                                            
+                                        </div>                                
+
+                                        <div class="md-form">
+                                            <i class="fa fa-lock prefix"  style="color:#314267;"></i>
+                                            <input placeholder="Contraseña" type="password" id="contrasenausuariologin" class="form-control white-text">
+                                        </div>
+
+                                        <div class="text-center" >
+                                            <button type = "submit" value = " Submit " class="btn btn-lg " style="background-color:#314267;">Entrar</button>                                        
+                                        </div>
+                                    </form>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+    </header>
+    <!--Navegación principal-->
+                
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.js"></script> 
+    <script>
+        new WOW().init();
+    </script>
+</body>
+</html>
